5e57493a2730e5ed1e3a88766a976005fa38a684
Hugo <127580302+HugoMGS13@users.noreply.github.com>
2024-11-28 01:12:58 -0300

    Update grafo_matriz.c

--- grafo_matriz.c
+++ grafo_matriz.c
@@ -4,7 +4,7 @@
 //Criando um valor máximo da matriz
 #define maximo 10
 
-//Criando a variável que irá armazenar a matriz
+//Criando a variável que irá armazenar o tamanhho da matriz
 int tamanho = 0;
 
 //Criando a matriz com o seu tamanho máximo
